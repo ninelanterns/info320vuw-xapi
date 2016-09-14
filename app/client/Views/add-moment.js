56c07616e725a0f4d022eea134e0314606f7b27b
Liam Murphy <fxsliam@outlook.com>
2016-09-14 18:28:35 +1200

    added event function to addmoment.js

--- app/client/Views/add-moment.js
+++ app/client/Views/add-moment.js
@@ -1,3 +1,10 @@
 Template.addmoment.onCreated(function (){
+  //code runs once template is loaded
 
+});
+Template.addmoment.events({
+  //example helper
+  // functionName() {
+  // code here
+  // },
 });
