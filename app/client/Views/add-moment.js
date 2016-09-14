8645422a1a2bd40c1ac9220825e6e829ab24d217
Liam Murphy <fxsliam@outlook.com>
2016-09-14 19:22:14 +1200

    added package Sessions and made relevant verbs appear once user has selected an activity

--- app/client/Views/add-moment.js
+++ app/client/Views/add-moment.js
@@ -1,3 +1,5 @@
+Session.set('verb', false);
+
 Template.addmoment.onCreated(function (){
   //code runs once template is loaded
 
@@ -5,6 +7,32 @@ Template.addmoment.onCreated(function (){
 Template.addmoment.events({
   //this function occurs whenever the user selects an activity
   'change #activities': function(a,b) {
+    //Arrays of verbs for the app
+    var Babybirth = ['select a verb','delivered','fed','bathed'];
+    var Meeting = ['select a verb','attended','participated'];
+    var Venepuncture = ['select a verb','Attempted','Completed','Failed'];
+
+    //gets the activity the user selected
     var selected = $('#activities').val();
+
+    //Sets verb session equal to selected activity
+    if (selected === 'Babybirth') {
+      Session.set('verb',Babybirth);
+    }
+    else if (selected === 'Meeting') {
+      Session.set('verb',Meeting);
+    }
+    else if (selected === 'Venepuncture') {
+      Session.set('verb',Venepuncture);
+    }
+    else {
+      Session.set('verb', false);
+    }
   }
+});
+Template.addmoment.helpers({
+  verbOptions: function() {
+    return Session.get('verb');
+  },
+
 });
