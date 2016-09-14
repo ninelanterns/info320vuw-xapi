de9e4414f108ac4a1cacc0a2938927e27c5c106c
Liam Murphy <fxsliam@outlook.com>
2016-09-14 19:50:43 +1200

    Implemented circumstances (context) changing depending on verb selected

--- app/client/Views/add-moment.js
+++ app/client/Views/add-moment.js
@@ -1,4 +1,5 @@
 Session.set('verb', false);
+Session.set('circumstance', false);
 
 Template.addmoment.onCreated(function (){
   //code runs once template is loaded
@@ -6,8 +7,8 @@ Template.addmoment.onCreated(function (){
 });
 Template.addmoment.events({
   //this function occurs whenever the user selects an activity
-  'change #activities': function(a,b) {
-    //Arrays of verbs for the app
+  'change #activities': function() {
+    //Arrays of verbs for the activities
     var Babybirth = ['select a verb','delivered','fed','bathed'];
     var Meeting = ['select a verb','attended','participated'];
     var Venepuncture = ['select a verb','Attempted','Completed','Failed'];
@@ -28,11 +29,37 @@ Template.addmoment.events({
     else {
       Session.set('verb', false);
     }
+  },
+  //this function occurs whenever the user selects a verb
+  'change #verb': function() {
+    //Arrays of circumstances for the verbs
+    var delivered = ['C-Section','Ventouse', 'Forceps', 'Vaginal'];
+    var attended = ['TypeOfMeeting1', 'TypeOfMeeting2'];
+    var Attempted = [];
+
+    //gets the verb the user selected
+    var selected = $('#verb').val();
+    console.log(selected);
+    if (selected === 'delivered') {
+      Session.set('circumstance', delivered);
+    }
+    else if (selected === 'attended') {
+      Session.set('circumstance', attended);
+    }
+    else if (selected === 'Attempted') {
+      Session.set('circumstance', Attempted);
+    }
+    else {
+      Session.set('circumstance', false);
+    }
   }
 });
 Template.addmoment.helpers({
   verbOptions: function() {
     return Session.get('verb');
   },
+  circumstanceOptions: function() {
+    return Session.get('circumstance');
+  }
 
 });
